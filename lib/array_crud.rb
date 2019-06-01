44573d707ee9a0d79f9cff22695727618fe1b158
Olga Rosas <olga.rosas@gmail.com>
2019-06-01 10:53:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,6 +8,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
+<<<<<<< HEAD
   array.push(element)
 end
 
@@ -29,9 +30,39 @@ end
 
 def retrieve_first_element_from_array(array)
   array[0]
+=======
+  my_array = []
+  my_array.push(my_array, "arrays!")
+end
+
+def add_element_to_start_of_array(array, element)
+  my_array = ["arrays!"]
+  my_array.unshift(my_array, "wow")
+end
+
+def remove_element_from_end_of_array(array)
+  my_array.pop
+end
+
+def remove_element_from_start_of_array(array)
+  my_array.shift
+end
+
+def retrieve_element_from_index(array, index_number)
+  my_array[0]
+
+end
+
+def retrieve_first_element_from_array(array)
+  my_array[0]
+>>>>>>> 724d5ab811e4a189aa2760043470580812ee5976
 
 end
 
 def retrieve_last_element_from_array(array)
+<<<<<<< HEAD
   array[-1]
+=======
+my_array[-1]
+>>>>>>> 724d5ab811e4a189aa2760043470580812ee5976
 end
